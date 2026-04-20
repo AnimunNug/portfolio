220b9edbf714428f78bd9ba58287c9c671fd4192
Animun4ik <nikita.animun@gmail.com>
2026-04-20 05:37:31 +0500

    hehe

--- script.js
+++ script.js
@@ -1,48 +1,9 @@
-const projectsData = [
-{ title: "Castle", img: "images/project1.jpg" },
-{ title: "Village", img: "images/project2.jpg" }
-];
-
-const modelsData = [
-{ title: "Sword", img: "images/project1.jpg" },
-{ title: "Tree", img: "images/project2.jpg" }
-];
-
-function render(containerId, data) {
-const container = document.getElementById(containerId);
-
-data.forEach(item => {
-const card = document.createElement("div");
-card.className = "card";
-
-card.innerHTML = `
-  <img src="${item.img}">
-  <span>${item.title}</span>
-`;
-
-card.onclick = () => openModal(item);
-
-container.appendChild(card);
-});
+function openPopup(img, text) {
+  document.getElementById("popup").style.display = "flex";
+  document.getElementById("popup-img").src = img;
+  document.getElementById("popup-text").innerText = text;
 }
 
-render("projects", projectsData);
-render("models", modelsData);
-
-/* MODAL */
-const modal = document.getElementById("modal");
-const modalImg = document.getElementById("modalImg");
-const caption = document.getElementById("caption");
-const close = document.getElementById("close");
-
-function openModal(item) {
-modal.style.display = "flex";
-modalImg.src = item.img;
-caption.innerText = item.title;
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
 }
-
-close.onclick = () => modal.style.display = "none";
-
-modal.onclick = (e) => {
-if (e.target === modal) modal.style.display = "none";
-};
